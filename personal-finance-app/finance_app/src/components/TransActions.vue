b7f34cbe2709a4aacb039b90947f38d93312244d
kdkaanev <kdkaane@github.io>
2025-03-26 07:12:48 +0200

    transaction -new

--- personal-finance-app/finance_app/src/components/TransActions.vue
+++ personal-finance-app/finance_app/src/components/TransActions.vue
@@ -1,5 +1,81 @@
 <template>
-  <h1>
-    Transactions
-  </h1>
+  <section class="container">
+    <div class="title">
+      <p>Transactions</p>
+    </div>
+  </section>
+
+  
 </template>
+
+
+<style scoped>
+.container{
+  /* Main Content */
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 32px 40px;
+gap: 32px;
+
+width: 1140px;
+height: 1135px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 1;
+z-index: 1;
+
+}
+.title {
+  /* Title */
+  /* Main Content Title */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 8px 0px;
+gap: 24px;
+
+width: 1060px;
+max-width: 1440px;
+height: 56px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+align-self: stretch;
+flex-grow: 0;
+}
+.title p {
+  /* Transactions */
+  /* Main Content Title */
+  /* Transactions */
+
+width: 198px;
+height: 38px;
+
+/* text-preset-1 */
+font-family: 'Public Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 32px;
+line-height: 120%;
+/* or 38px */
+
+color: #201F24;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+}
+
+</style>
