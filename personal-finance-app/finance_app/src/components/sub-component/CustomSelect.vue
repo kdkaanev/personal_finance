992719b7cb8b5da86c2f6ad5bc57703bca9661cc
kdkaanev <kdkaane@github.io>
2025-05-02 22:28:24 +0300

    add budget form done

---
+++ personal-finance-app/finance_app/src/components/sub-component/CustomSelect.vue
@@ -0,0 +1,358 @@
+<script>
+import data from '../../data/data.json';
+
+export default {
+  data() {
+    return {
+      budgets: data.budgets,
+      selectedBudget: data.budgets[0],
+      isOpen: false
+    };
+  },
+  methods: {
+    selectBudget(budget) {
+      this.selectedBudget = budget;
+      this.isOpen = false;
+    },
+
+  getColorName(hex) {
+    const colorMap = {
+        '#F2CDAC': 'Navy',
+        '#277C78': 'Green',
+        '#826cb0': 'Purple',
+        '#93674f': 'Brown',
+        '#934f6f': 'Magneta',
+        '#82C9D7': 'Blue',
+        '#626070': 'Grey',
+        '#7f9161': 'Army',
+        '#af81ba': 'Pink',
+        '#cab361': 'Yellow',
+
+
+
+      // Add more mappings as needed
+    };
+    return colorMap[hex] || 'Unknown';
+  }
+
+
+  }
+};
+</script>
+
+
+
+<template>
+    <section class="theme">
+        <h3 class="label">Theme</h3>
+        <div>
+        
+        <div @click="isOpen = !isOpen" class="select-dropdown">
+        
+            <section class="info">
+                <span 
+       class="dot" 
+       :style="{ backgroundColor: selectedBudget?.theme || '#ccc' }"
+     ></span>
+     {{ getColorName(selectedBudget?.theme) }}
+            </section>
+   
+     
+     <span class="caret"><img src="../../assets/icons/icon-caret-down.svg" alt="down"></span>
+
+        </div>
+  
+      <ul v-if="isOpen" class="dropdown-list">
+        <li 
+          v-for="budget in budgets" 
+          :key="budget.category" 
+          @click="selectBudget(budget)"
+          class="dropdown-item"
+        >
+        
+           <span class="name-theme">
+            <div class="dot" :style="{ backgroundColor: budget.theme }"></div>
+            {{ getColorName(budget.theme) }}
+           </span>
+    
+       
+            <span class="status"><img src="../../assets/icons/icon-selected.svg" alt=""></span>
+        
+        </li>
+      </ul>
+    </div>
+    </section>
+ 
+  </template>
+  
+
+  <style scoped>
+  .name-theme {
+   /* Frame 574 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 0px;
+gap: 12px;
+/* Green */
+
+width: 412px;
+height: 21px;
+
+/* text-preset-4 */
+font-family: 'Public Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 150%;
+/* identical to box height, or 21px */
+
+color: #201F24;
+
+
+/* Inside auto layout */
+flex: none;
+order: 2;
+flex-grow: 1;
+
+
+
+height: 21px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 1;
+
+  }
+  .info-panel {
+    /* Input Field/Field States */
+    /* Frame 574 */
+    /* Green */
+
+width: 396px;
+height: 21px;
+
+/* text-preset-4 */
+font-family: 'Public Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 150%;
+/* identical to box height, or 21px */
+
+color: #201F24;
+
+
+/* Inside auto layout */
+flex: none;
+order: 2;
+flex-grow: 1;
+
+  }
+  .info {
+    /* Input Field/Field States */
+    /* Frame 574 */
+
+/* Auto layout */
+font-family: 'Public Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 150%;
+/* identical to box height, or 21px */
+
+color: #201F24;
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 0px;
+gap: 12px;
+
+width: 424px;
+height: 21px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 1;
+    }
+  .select-dropdown {
+    /* Input Field/Field States */
+
+box-sizing: border-box;
+
+/* Auto layout */
+display: flex;
+justify-content: space-between;
+flex-direction: row;
+align-items: center;
+padding: 12px 20px;
+gap: 16px;
+cursor: pointer;
+
+width: 496px;
+height: 45px;
+
+background: #FFFFFF;
+border: 1px solid #98908B;
+border-radius: 8px;
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+align-self: stretch;
+flex-grow: 0;
+    }
+    .theme {
+      /* Input Field/Input Fields */
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 4px;
+
+width: 496px;
+height: 67px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 2;
+align-self: stretch;
+flex-grow: 0;
+    }
+.label {
+  /* Label */
+  /* Color Tag */
+
+
+height: 18px;
+
+/* text-preset-5-bold */
+font-family: 'Public Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 12px;
+line-height: 150%;
+/* identical to box height, or 18px */
+
+color: #696868;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+align-self: stretch;
+flex-grow: 0;
+
+}
+  .dropdown {
+    /* Input Field/Field States */
+
+box-sizing: border-box;
+
+/* Auto layout */
+display: inline-block;
+position: relative;
+flex-direction: row;
+align-items: center;
+padding: 12px 20px;
+gap: 16px;
+
+
+height: 45px;
+
+background: #FFFFFF;
+border: 1px solid #98908B;
+border-radius: 8px;
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+align-self: stretch;
+flex-grow: 0;
+
+  }
+  
+ 
+  
+  .dropdown-list {
+    position: absolute;
+    /* All Color */
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: flex-start;
+padding: 12px 20px;
+gap: 12px;
+width: 100%;
+overflow-y: auto;
+max-height: 160px;
+
+position: relative;
+
+
+
+background: #FFFFFF;
+/* drop-shadow */
+box-shadow: 0px 4px 24px rgba(0, 0, 0, 0.25);
+border-radius: 8px;
+
+    z-index: 10;
+  }
+  
+  .dropdown-item {
+    padding: 6px 10px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    /* Frame 576 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+padding: 0px;
+gap: 16px;
+
+
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+align-self: stretch;
+flex-grow: 0;
+
+  }
+  
+  .dropdown-item:hover {
+    background: #f0f0f0;
+  }
+  
+  .dot {
+  /* Color Tag */
+
+width: 16px;
+height: 16px;
+
+
+border-radius: 50%;
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+  }
+  </style>
+  
--- personal-finance-app/finance_app/src/components/sub-component/ModalPop.vue
+++ personal-finance-app/finance_app/src/components/sub-component/ModalPop.vue
@@ -1,13 +1,17 @@
 <!-- src/components/BudgetModal.vue -->
 <script>
 
-import data from '../../data/data.json '
+import data from'../../data/data.json'
+import CustomSelect from './CustomSelect.vue'
 export default {
+  components: {
+    CustomSelect
+  },
   name: 'BudgetModal',
   data() {
     return {
       budgets: data.budgets,
-      selectedCategory: '',
+      selectedCategory: 'Entertainment',
       selectedTheme: '',
       categories: [
           'Entertainment' ,
@@ -44,15 +48,20 @@ export default {
 <template>
     <div class="modal-backdrop" @click.self="$emit('close')">
       <div class="modal-content">
-        <h2 class="text-xl font-bold mb-4">Add New Budget</h2>
-        <p class="text-sm text-gray-500 mb-4">Choose a category to set a spending budget. These categories can help you monitor spending.</p>
-        <form @submit.prevent="submitBudget" class="space-y-3">
+        <section class="action">
+          <h1>Add New Budget</h1>
+          <button class="btn-secondary" @click="$emit('close')">
+            <img src="../../assets/icons/icon-close-modal.svg" alt="close">
+          </button>
+        </section>
+        <p class="text-sm">Choose a category to set a spending budget. These categories can help you monitor spending.</p>
+        <form @submit.prevent="submitBudget" class="space">
           <div class="category">
             <label for="category"  >Budget Category</label>
           <select name="category" id="category" v-model="selectedCategory">
 
-            <option v-for="category in  categories " :key="category" :value="category" >
-            {{ category }}
+            <option v-for="budget in  budgets " :key="budget" :value="budget" >
+              {{ budget.category }}
           </option>
           </select>
           </div>
@@ -61,11 +70,9 @@ export default {
             <input type="number" id="maximum" v-model="amount" class="input" placeholder="$ e.g.2000" required>
           </div>
           <div class="theme">
-            <label for="theme">Theme</label>
-            <select name="theme" id="theme" v-model="selectedTheme">
-              
-            </select>
+            <CustomSelect :budgets="budgets" v-model="selectedTheme" />
           </div>
+          <button type="submit" class="btn-primary">Add Budget</button>
          
          
         </form>
@@ -76,22 +83,227 @@ export default {
 
   
   <style scoped>
+  .maximum input {
+    /* Maximum Spend */
+    /* Content */
+    /* Input Field/Input Fields */
+    /* Budget Name */
+    /* Input Field/Field States */
+
+
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 12px 20px;
+gap: 16px;
+cursor: pointer;
+
+
+width: 496px;
+height: 45px;
+
+background: #FFFFFF;
+border: 1px solid #98908B;
+border-radius: 8px;
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+align-self: stretch;
+flex-grow: 0;
+  }
+  select {
+    /* Input Field/Field States */
+
+box-sizing: border-box;
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 12px 20px;
+gap: 16px;
+
+width: 496px;
+height: 45px;
+cursor: pointer;
+
+background: #FFFFFF;
+border: 1px solid #98908B;
+border-radius: 8px;
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+align-self: stretch;
+flex-grow: 0;
+
+  }
+  label {
+    /* Budget Category */
+    /* Content */
+    /* Input Field/Input Fields */
+    /* Budget Name */
+
+width: 496px;
+height: 18px;
+
+/* text-preset-5-bold */
+font-family: 'Public Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 12px;
+line-height: 150%;
+/* identical to box height, or 18px */
+
+color: #696868;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+align-self: stretch;
+flex-grow: 0;
+  }
+  .category {
+    /* Category */
+    /* Content */
+    /* Input Field/Input Fields */
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 4px;
+
+width: 496px;
+height: 67px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+align-self: stretch;
+flex-grow: 0;
+  }   
+  .space {
+    /* Space */
+    /* Content */
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 16px;
+
+width: 496px;
+height: 233px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 2;
+align-self: stretch;
+flex-grow: 0;
+
+  }
+  .text-sm {
+  /* Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus  hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet. */
+
+width: 496px;
+height: 42px;
+
+/* text-preset-4 */
+font-family: 'Public Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 150%;
+/* or 21px */
+
+color: #696868;
+
+
+/* Inside auto layout */
+
+
+  }
+  
+  .action h1 {
+  /* Add New Budget */
+
+
+height: 38px;
+
+/* text-preset-1 */
+font-family: 'Public Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 32px;
+line-height: 120%;
+/* or 38px */
+
+color: #201F24;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+  }
+  .action {
+    /* title */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+padding: 0px;
+gap: 20px;
+
+width: 496px;
+height: 38px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+align-self: stretch;
+flex-grow: 0;
+
+  }
   .modal-backdrop {
     position: fixed;
     inset: 0;
     background: rgba(0, 0, 0, 0.4);
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     z-index: 50;
   }
+ 
   .modal-content {
+    display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 32px;
+gap: 20px;
     background: white;
     border-radius: 1rem;
     padding: 2rem;
-    width: 90%;
-    max-width: 400px;
+    width: 560px;
+    max-width: 560px;
+    height: 490px;
     box-shadow: 0 10px 30px rgba(0, 0, 0, 0.2);
+
+
   }
   .input {
     width: 100%;
@@ -100,15 +312,64 @@ export default {
     border-radius: 0.375rem;
   }
   .btn-primary {
-    background: #2563eb;
-    color: white;
-    padding: 0.5rem 1rem;
-    border-radius: 0.375rem;
+  /* Button/Primary */
+
+/* Auto layout */
+/* Button/Primary */
+
+/* Auto layout */
+/* Frame 529 */
+
+box-sizing: border-box;
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 16px;
+gap: 16px;
+
+width: 496px;
+height: 53px;
+
+background: #201F24;
+border-radius: 8px;
+cursor: pointer;
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 1;
+/* Add Budget */
+
+
+
+/* text-preset-4-bold */
+font-family: 'Public Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 14px;
+line-height: 150%;
+/* identical to box height, or 21px */
+
+color: #FFFFFF;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+
+
   }
   .btn-secondary {
-    background: #e5e7eb;
+    
     padding: 0.5rem 1rem;
     border-radius: 0.375rem;
+    cursor: pointer;
   }
   </style>
   
